465d41c74aedb51f1f348f271bb0c662209b5b6e
Arham <arham21st@gmail.com>
2023-08-23 01:04:44 +0500

    folder structure

---
+++ lib/src/constants/colors/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+/* -- LIST OF ALL COLORS -- */
+
+const primaryColor = Color(0xFFFFE400);
+const secondaryColor = Color(0xFF272727);
+const accentColor = Color(0xFF001BFF);
+
+const whiteColor = Colors.white;
+const darkColor = Color(0xff000000);
+const cardBgColor = Color(0xFFF7F6F1);
+
+
+
---
+++ lib/src/constants/text_strings/app_texts.dart
@@ -0,0 +1,19 @@
+/* -- App Text Strings -- */
+
+// -- GLOBAL Texts
+const String tNext = "Next";
+const String tLogin = "Login";
+const String tEmail = "E-Mail";
+const String tSignup = "Signup";
+const String tPhoneNo = "Phone No";
+const String tPassword = "Password";
+const String tFullName = "Full Name";
+const String tForgetPassword = "Forget Password";
+const String tSignInWithGoogle = "Sign-In with Google";
+
+
+
+
+
+
+
